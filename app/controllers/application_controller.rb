32dc4c5c03e12c8ce7eec7e32d2fe86e10c40b23
Johan Brissmyr <brissmyr@gmail.com>
2018-12-29 18:15:54 +0100

    Flash when told to. Emulates a failed login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,6 +19,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  before_action do
+    flash.alert = 'Invalid Email or password.' if params[:failed_error]
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
