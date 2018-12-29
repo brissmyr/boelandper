1023167835e4ac291449df62984e52d2263b7965
Johan Brissmyr <brissmyr@gmail.com>
2018-12-29 16:57:55 +0100

    Include railtie

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module TazkiyahMvp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
     require 'castle/middleware/railtie'
   end
 end
