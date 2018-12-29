ef30771e33b5de6268a810ccd1b6e53496706f8e
Johan Brissmyr <brissmyr@gmail.com>
2018-12-29 14:01:38 +0100

    Dummy deny hijack at /login

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require_relative '../app/middleware/castle_kid'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
