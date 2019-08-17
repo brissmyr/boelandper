f44560194a6852b95decd034711647f7ace0e644
Johan Brissmyr <brissmyr@gmail.com>
2019-08-17 23:17:38 +0200

    Set user ID header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,6 +23,12 @@ class ApplicationController < ActionController::Base
     flash.alert = 'Invalid Email or password.' if params[:failed_error]
   end
 
+  after_action do
+    if current_user && !response.headers['X-User-Id']
+      response.headers['X-User-Id'] = current_user.id
+    end
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
